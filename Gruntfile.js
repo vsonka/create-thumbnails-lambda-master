7bb5248a8ad05e4112406e9e0fcaa65f9285f4f7
vsonka <sonkarvikas0786@gmail.com>
2018-05-14 17:53:02 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ grunt.initConfig({
 	},
 	lambda_deploy: {
     	default: {
-    		arn: 'arn:aws:lambda:us-east-1:1234567890123:function:CreateThumbnail'
+    		arn: 'arn:aws:lambda:us-east-1:845068533700:function:CreateThumbnail'
     	}
     },
 	lambda_package: {
